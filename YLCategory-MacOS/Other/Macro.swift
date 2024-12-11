95d03257cd9f5b97b760315d07ae328121cfdb00
魏宇龙 <weiyulong1987@163.com>
2024-12-11 18:01:57 +0800

    fix

--- YLCategory-MacOS/Other/Macro.swift
+++ YLCategory-MacOS/Other/Macro.swift
@@ -20,9 +20,9 @@ public let GreenColor: NSColor = .green
 public let OrangeColor: NSColor = .orange
 public let YellowColor: NSColor = .yellow
 public let BlueColor: NSColor = .blue
-public var SystemBlueColor: NSColor = .systemBlue
-public var ControlAccentColor: NSColor = .controlAccentColor
-public var RandomColor: NSColor = NSColor(red: CGFloat(arc4random() % 255) / 255.0, green: CGFloat(arc4random() % 255) / 255.0, blue: CGFloat(arc4random() % 255) / 255.0, alpha: 1.0)
+public let SystemBlueColor: NSColor = .systemBlue
+public var ControlAccentColor: NSColor { .controlAccentColor }
+public var RandomColor: NSColor { NSColor(red: CGFloat(arc4random() % 255) / 255.0, green: CGFloat(arc4random() % 255) / 255.0, blue: CGFloat(arc4random() % 255) / 255.0, alpha: 1.0) }
 
 public var RGBA:(CGFloat, CGFloat, CGFloat, CGFloat) -> NSColor  = { r, g, b, a in
     NSColor(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: a)
@@ -40,35 +40,35 @@ public var WhiteColoAlpha: (CGFloat) -> NSColor = { NSColor(white: 1, alpha: $0)
 
 // MARK: - 屏幕
 
-public var kScreenScale: CGFloat = NSScreen.main?.backingScaleFactor ?? 0.0
-public var kScreenWidth: CGFloat = NSScreen.main?.frame.size.width ?? 0.0
-public var kScreenHeight: CGFloat = NSScreen.main?.frame.size.height ?? 0.0
-public var kStatusBarHeight: CGFloat = NSApp.mainMenu?.menuBarHeight ?? 0.0
+public var kScreenScale: CGFloat { NSScreen.main?.backingScaleFactor ?? 0.0 }
+public var kScreenWidth: CGFloat { NSScreen.main?.frame.size.width ?? 0.0 }
+public var kScreenHeight: CGFloat { NSScreen.main?.frame.size.height ?? 0.0 }
+public var kStatusBarHeight: CGFloat { NSApp.mainMenu?.menuBarHeight ?? 0.0 }
 
 // MARK: - 字体
 
-public var Font:(CGFloat) -> NSFont = { .systemFont(ofSize: $0) }
+public var Font: (CGFloat) -> NSFont = { .systemFont(ofSize: $0) }
 public var BoldFont: (CGFloat) -> NSFont = { .boldSystemFont(ofSize: $0) }
-public var MediumFont: (CGFloat) -> NSFont = {.systemFont(ofSize: $0, weight: .medium) }
-
+public var MediumFont: (CGFloat) -> NSFont = { .systemFont(ofSize: $0, weight: .medium) }
+public var ThinFont: (CGFloat) -> NSFont = { .systemFont(ofSize: $0, weight: .thin) }
 
 // MARK: - app相关信息
 
 // app是沙盒
 public let kAppIsSanbox: Bool = ProcessInfo.processInfo.environment["APP_SANDBOX_CONTAINER_ID"] != nil
 // app是暗黑模式
-public var kAppIsDarkTheme: Bool = NSApp.effectiveAppearance.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua
+public var kAppIsDarkTheme: Bool { NSApp.effectiveAppearance.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua }
 // 系统是暗黑模式
-public var kSystemIsDarkTheme: Bool = {
+public var kSystemIsDarkTheme: Bool {
     let info = UserDefaults.standard.persistentDomain(forName: UserDefaults.globalDomain)
     if let style = info?["AppleInterfaceStyle"] as? String {
         return style.caseInsensitiveCompare("dark") == .orderedSame
     }
     return false
-}()
+}
 
-public var kDocumentPath: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last ?? ""
-public var kCachePath: String = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).last ?? ""
+public var kDocumentPath: String { NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last ?? "" }
+public var kCachePath: String { NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).last ?? "" }
 public var BundlePath: (String) -> String? = { Bundle.main.path(forResource: $0, ofType: nil) }
 
 public let kAPP_Version: String = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
